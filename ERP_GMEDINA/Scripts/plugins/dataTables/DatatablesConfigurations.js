8b78d5dc2ff364635e484b2416fefb638ce6a5ea
chris43123 <46337972+chris43123@users.noreply.github.com>
2020-01-26 01:24:02 -0600

    Databales rrefencaia a variable entre docmentos

--- ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
+++ ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
@@ -2,6 +2,29 @@
 var Admin = false;
 var textoBoton = 'Mostrar activos';
 var botones = [];
+var language = n = {
+    "sProcessing": "Procesando...",
+    "sLengthMenu": "Mostrar _MENU_ registros",
+    "sZeroRecords": "No se encontraron resultados",
+    "sEmptyTable": htmlSpiner,
+    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+    "sInfoPostFix": "",
+    "sSearch": "Buscar:",
+    "sUrl": "",
+    "sInfoThousands": ",",
+    "sLoadingRecords": "Cargando...",
+    "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+    }, "oAria": {
+        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+    }
+};
 var htmlSpiner =
     `<div class="sk-spinner sk-spinner-wave">
                 <div class="sk-rect1"></div>
